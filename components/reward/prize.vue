4634f8b795b0731bb7a3be7440ff8427b2ee2c3d
ss0n1c <lls.s0n1cll@gamil.com>
2025-08-01 16:33:52 +0300

    add design on reward page

--- components/reward/prize.vue
+++ components/reward/prize.vue
@@ -14,7 +14,9 @@ onMounted(async()=>{
   }
 })
 async function buyReward(rewardScore,rewardID){
-    if(useUserData.data>=rewardScore){
+    let conf = confirm("are you sure?")
+    if(conf){
+        if(useUserData.data>=rewardScore){
         const userScore = useUserData.data - rewardScore
         const oldUserRewID = useUserData.rewardID
         const newUserRewID = [...oldUserRewID,rewardID]
@@ -25,13 +27,18 @@ async function buyReward(rewardScore,rewardID){
     } else {
         alert("you are too poore")
     }
+    }
+
 }
 
 </script>
 
 <template>
-    <div class="flex justify-around items-center bg-[white] min-h-screen rounded-t-[20px]">
-        <div class="rewardField flex justify-around items-center gap-x-[20px] min-w-full ">
+    <div class="flex rewardSection p-8 gap-y-[20px] flex-col items-center min-h-screen rounded-t-[20px]">
+        <div>
+            <h2>choose your reward</h2>
+        </div>
+        <div class="rewardField flex  justify-around items-center gap-x-[20px] min-w-full ">
             <div class="rewardCard" v-for="rewardItem in getRewardList"
             :key="rewardItem.$id">
                 <div class="rewardCard__title">{{rewardItem.name}}</div>
@@ -52,31 +59,80 @@ async function buyReward(rewardScore,rewardID){
 
 
 <style scoped lang="scss">
+.rewardSection{
+    background: $backBLue;
+    position:relative;
+    border:2px rgba(39, 39, 150, 0.395) solid;
+    &::after{
+    content:"";
+    position: absolute;
+    border:1px $gold solid;
+    top:7px;
+    left:7px;
+    right: 7px;
+    bottom:7px;
+    border-radius: 20px;
+    pointer-events: none;
+  }
+  & h2{
+    @include font($playFair,700,28px,32px,$gold);
+    letter-spacing: 2px;
+    text-transform: capitalize;
+  }
+}
 .rewardCard{
     @include flex;
+    @include font($playFair,500,24px,28px,$gold);
     flex-direction: column;
-    border: 2px black solid;
+    border: 2px $gold solid;
     padding: 10px 20px;
     border-radius: 10px;
     row-gap: 15px;
     font-size: 20px;
 
+    &:nth-of-type(odd){
+        border: 2px $blue solid;
+    }
+
     &__price{
         width: 100%;
         height: 100%;
         button{
+            border:1px $gold solid;
+            font-family: $serif;
+            font-weight: 700;
+            box-shadow: 5px 4px 4px black;
+            text-shadow: 3px 3px 3px $brown;
+            padding:5px 10px;
             width: 100%;
             height: 100%;
-            background-color: green;
+            background:#008000;
             border-radius: 10px;
-            &:hover{
-                background-color: blue;
+            position: relative;
+            &::after{
+                position:absolute;
+                content:" ";
+                border:1px $gold solid;
+                top:2px;
+                left:2px;
+                right: 2px;
+                bottom:2px;
+                border-radius: 10px;
+                pointer-events: none;
             }
         }
     }
     &__buyed{
+        text-shadow: 3px 3px 3px $brown;
+        padding:5px 10px;
         min-width:100%;
-        background-color: blue;
+        background:$lighBlue;
+        border-radius: 10px;
+        text-align:center;
+        border:1px $gold solid;
+        box-shadow: 2px 2px 2px $gold;
+        text-transform: uppercase;
+        font-size: 16px;
     }
 }
 </style>
